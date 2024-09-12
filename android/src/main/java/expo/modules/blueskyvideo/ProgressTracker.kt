df167182d2357ae4dc4081f7990a0e724380e4e8
Hailey <me@haileyok.com>
2024-09-12 16:11:17 -0700

    lint kt

--- android/src/main/java/expo/modules/blueskyvideo/ProgressTracker.kt
+++ android/src/main/java/expo/modules/blueskyvideo/ProgressTracker.kt
@@ -4,31 +4,33 @@ import android.os.Handler
 import android.os.Looper
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
-import kotlin.math.floor
 import expo.modules.kotlin.viewevent.ViewEventCallback
+import kotlin.math.floor
 
 @androidx.annotation.OptIn(UnstableApi::class)
 class ProgressTracker(
-  private val player: Player,
-  private val onTimeRemainingChange: ViewEventCallback<Map<String, Any>>
+    private val player: Player,
+    private val onTimeRemainingChange: ViewEventCallback<Map<String, Any>>,
 ) : Runnable {
-  private val handler: Handler = Handler(Looper.getMainLooper())
+    private val handler: Handler = Handler(Looper.getMainLooper())
 
-  init {
-    handler.post(this)
-  }
+    init {
+        handler.post(this)
+    }
 
-  override fun run() {
-    val currentPosition = player.currentPosition
-    val duration = player.duration
-    val timeRemaining = floor(((duration - currentPosition) / 1000).toDouble())
-    onTimeRemainingChange(mapOf(
-      "timeRemaining" to timeRemaining
-    ))
-    handler.postDelayed(this, 1000)
-  }
+    override fun run() {
+        val currentPosition = player.currentPosition
+        val duration = player.duration
+        val timeRemaining = floor(((duration - currentPosition) / 1000).toDouble())
+        onTimeRemainingChange(
+            mapOf(
+                "timeRemaining" to timeRemaining,
+            ),
+        )
+        handler.postDelayed(this, 1000)
+    }
 
-  fun remove() {
-    handler.removeCallbacks(this)
-  }
+    fun remove() {
+        handler.removeCallbacks(this)
+    }
 }
