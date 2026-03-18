38941144e32779c0be2f6bc669aaa5b3e9b11d07
Samuel Newman <mozzius@protonmail.com>
2026-03-18 17:46:09 +0200

    fix moveIt.sh script

--- moveIt.sh
+++ moveIt.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-yarn prepare && rm -Rf ../social-app/node_modules/@bsky.app/bluesky-video && cp -R . ../social-app/node_modules/@bsky.app/bluesky-video
+yarn prepare && rm -Rf ../social-app/node_modules/@bsky.app/video && cp -R . ../social-app/node_modules/@bsky.app/video
